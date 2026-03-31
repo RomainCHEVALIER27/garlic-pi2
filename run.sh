c64340ad1d07765e05ff54081200b89d3fb553c6
RomainCHEVALIER27 <romain.chevalier.27@gmail.com>
2026-03-31 07:23:14 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-
-# 1. On donne les accès au matériel vidéo du Raspberry Pi
+# Autoriser l'accès à l'écran
 sudo chmod 666 /dev/tty0 /dev/fb0 > /dev/null 2>&1
 
-# 2. On lance l'affichage via le serveur graphique xinit
-# Cette URL est une horloge fiable qui fonctionne sans erreur 404
+# Lancer l'affichage (On pointe vers ton GitHub Pages !)
 xinit /usr/bin/chromium-browser \
     --kiosk \
     --no-sandbox \
     --test-type \
     --noerrdialogs \
     --disable-infobars \
-    https://jan.becke.online/flipclock/ \
+    https://romainchevalier27.github.io/garlic-pi2/ \
     -- -nocursor
